c9cec7de657704e41dd9e105052be6b96524ba49
Asma Kolaib <as.kolaib@gmail.com>
2020-08-16 18:53:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+class Polygon {
+  constructor(sides) {
+    this.sides = sides;
+  }
+  get countSides() {
+    return this.sides.length;
+  }
+  get perimeter() {
+    let sum = 0;
+    sides.forEach( item =>  sum += this.item);
+    return sum;
+  }
+}
+let polygon = new Polygon([5, 5, 5]);
+polygon.countSides;
+console.log(polygon.perimeter);
